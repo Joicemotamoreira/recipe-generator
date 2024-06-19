fcf0249b837428bd528cac7a3333e5e65ee73c21
Joicemotamoreira <joicemotamoreira@live.com>
2024-06-19 11:26:42 +0100

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
+  console.log("recipe generetated");
   new Typewriter("#recipe", {
-    strings: "new recipe",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let ingredient = document.querySelector("#ingredient");
+  let apiKey = "b5f48bboa0a3000997064tb1d30c4a9f";
+  let context =
+    "you are sandwich maker that loves to help the user to make simple sandwiches providing a easy sandwich recipe. make sure use the user ingredient.";
+  let prompt = `User ingredient: generate one sandwich recipe about ${ingredient.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating recipe");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeGeneretor = document.querySelector("#recipe-generator");
