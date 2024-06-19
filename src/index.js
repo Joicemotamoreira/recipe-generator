d25e50233d26266b6707e5bce620d4fbab9e5f5b
Joicemotamoreira <joicemotamoreira@live.com>
2024-06-19 16:45:10 +0100

    Updated CSS

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function generateRecipe(event) {
 
   let recipeElement = document.querySelector("#recipe");
   recipeElement.classList.remove("hidden");
-  recipeElement.innerHTML = `<span class="generating">⏳ Generating recipe with <i>${ingredient.value}</i> for you!</span>`;
+  recipeElement.innerHTML = `<span class="generating">⏳ Generating recipe with <i>${ingredient.value}</i> for you! 😋</span>`;
 
   axios.get(apiUrl).then(displayRecipe);
 }
